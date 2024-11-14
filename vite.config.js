f75c67234a1c676272e0c58888c30e2835a67617
Patrick Solis <patri@ASUS-Patrick.(none)>
2024-11-13 22:30:03 -0600

    Github Pages Version

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/E-Commerce/', // Set this to your repository name for GitHub Pages
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/react_app/App.jsx'],
